d81d60d0e502098ce996caac683d72b440572f2f
Kudzanai <kudzanaichakavarika67@gmail.com>
2025-08-02 17:39:30 +0200

    Initial commit

--- Firetracker-backendd/settings.py
+++ Firetracker-backendd/settings.py
@@ -161,7 +161,15 @@ USE_TZ = True
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
 
 
 
