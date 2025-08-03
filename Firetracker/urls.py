84e4e8b0e634a3f8c702244cafcf470b251bac42
Kudzanai <kudzanaichakavarika67@gmail.com>
2025-08-03 13:04:28 +0200

    Fix GDAL dependency for Render

--- Firetracker/urls.py
+++ Firetracker/urls.py
@@ -1,17 +1,9 @@
-# Firetracker/urls.py
-from django.urls import path
-from . import views
-from django.views.decorators.cache import never_cache
-
 from django.urls import path
 from . import views
 from django.views.decorators.cache import never_cache
 
 urlpatterns = [
-    # Root endpoint
-    path('', views.home, name='home'),
-    
-    # Data endpoints
+  
     path('api/provinces/', never_cache(views.provinces_json), name='provinces_json'),
     path('api/districts/', never_cache(views.districts_json), name='districts_json'),
     path('api/firepoints/', never_cache(views.firepoints_json), name='firepoints_json'),
